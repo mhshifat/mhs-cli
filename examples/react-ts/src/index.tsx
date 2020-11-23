3de4ed5d09304122bbd4217eab8e346060797d12
mhshifat <mehedihassanshifat01@gmail.com>
2020-11-24 01:28:43 +0600

    Added typescript support for react project generator 📦

---
+++ examples/react-ts/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+
+const root = (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
+
+ReactDOM.render(root, document.getElementById("root"));
--- src/helpers/reactProjectBuild.ts
+++ src/helpers/reactProjectBuild.ts
@@ -72,7 +72,57 @@ export default async ({
         }
       );
     } else {
-      spinner.text = "Generate Typescript react project...";
+      exec(
+        `npx create-react-app ${folderName} --template=typescript`,
+        (error: ExecException | null, stdout: string, stderr: string) => {
+          if (error) {
+            spinner?.fail(chalk.red("Failed to generate your react app 😢"));
+            return;
+          }
+          exec(`rm -rf ${folderName}/src`);
+          exec(`rm -rf ${folderName}/public`);
+          exec(
+            `cd ${folderName} && ${
+              packageManager === "npm"
+                ? "npm i"
+                : packageManager === "yarn"
+                ? "yarn add"
+                : "npm i"
+            } react-router-dom @types/react-router-dom`
+          );
+          const source = path.join(
+            __dirname,
+            "..",
+            "..",
+            "examples",
+            "react-ts"
+          );
+          const destination = folderName;
+          fs.copy(source, destination, async (err) => {
+            if (err) {
+              console.log(err);
+              spinner?.fail(chalk.red("Failed to generate your react app 😢"));
+              process.exit(1);
+            } else {
+              try {
+                const packageJSON = require(path.join(
+                  process.cwd() + "/" + folderName + "/package.json"
+                ));
+                await fs.writeFile(
+                  path.join(process.cwd() + "/" + folderName + "/package.json"),
+                  JSON.stringify(
+                    { ...packageJSON, name: projectName || packageJSON.name },
+                    null,
+                    2
+                  ),
+                  "utf8"
+                );
+              } catch (err) {}
+              spinner?.succeed("All set 👌");
+            }
+          });
+        }
+      );
     }
   }
 };
